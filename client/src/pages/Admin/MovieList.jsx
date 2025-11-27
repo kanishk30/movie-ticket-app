2639aa63a4b03f46015d7fdc9857c8afd20dcfb2
kanishk30 <kanishk71@gmail.com>
2025-11-27 07:56:33 +0530

    Movie List table

--- client/src/pages/Admin/MovieList.jsx
+++ client/src/pages/Admin/MovieList.jsx
@@ -1,11 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Table } from "antd";
+import { getAllMovies } from "../../backend/movie";
+import moment from "moment";
 
 const MovieList = () => {
-  return (
-    <div>
-      <h2>Movie list</h2>
-    </div>
-  );
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await getAllMovies();
+      console.log(response.data, "Movielist");
+      setMovies(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const tableHeadings = [
+    {
+      title: "Poster",
+      dataIndex: "poster",
+      render: (value, record, index) => {
+        return <img src={record.posterPath} height={100} width={100} />;
+      },
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+    },
+    {
+      title: "Language",
+      dataIndex: "language",
+    },
+    {
+      title: "Genre",
+      dataIndex: "genre",
+    },
+    {
+      title: "Release Date",
+      dataIndex: "releaseDate",
+      render: (value, record) => {
+        return moment(record.releaseDate).format("DD-MM-YYYY");
+      },
+    },
+
+    {
+      title: "Duration",
+      dataIndex: "duration",
+      render: (value, record, i) => {
+        return `${value}min`;
+      },
+    },
+    {
+      title: "Ratings",
+      dataIndex: "rating",
+    },
+  ];
+
+  return <Table dataSource={movies} columns={tableHeadings} />;
 };
 
 export default MovieList;
