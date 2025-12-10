ff747e0cfa4c6b162681905b6e46a89a67dd0a2a
kanishk30 <kanishk71@gmail.com>
2025-12-10 07:31:19 +0530

    SIngle movie UI

--- client/src/pages/SingleMovie.jsx
+++ client/src/pages/SingleMovie.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getSingleMovie } from "../backend/movie";
-import { Card, Col, Row, Image, Typography } from "antd";
+import { Card, Col, Row, Image, Typography, Tag, Rate, Button } from "antd";
+import moment from "moment";
 
 const { Title } = Typography;
 
@@ -31,12 +32,39 @@ const SingleMovie = () => {
     <div>
       <Card>
         <Row gutter={16}>
-          <Col>
-            <Image src={movie.posterPath} alt={movie.title} />
+          <Col xs={24} sm={16}>
+            <Image
+              src={movie.posterPath}
+              alt={movie.title}
+              style={{ width: "100%", height: "500px", borderRadius: 8 }}
+            />
           </Col>
-          <Col>
-            <Title>{movie.title}</Title>
-            {/* to add other fields.. */}
+          <Col xs={24} sm={16}>
+            <Title level={4}>{movie.title}</Title>
+
+            <Tag style={{ marginBottom: 4 }}>{movie.genre}</Tag>
+            {movie.rating ? (
+              <>
+                <Rate allowHalf count={10} disabled value={movie.rating} />
+                <span style={{ marginLeft: 8 }}>{movie.rating}</span>
+              </>
+            ) : null}
+            <p style={{ marginBottom: 12 }}>{movie.description}</p>
+
+            <strong>Release Date: </strong>
+            <span>{moment(movie.releaseDate).format("DD-MM-YYYY")}</span>
+            <div>
+              <strong>Movie Language: </strong>
+              <span>{movie.language}</span>
+            </div>
+            <div>
+              <strong>Duration: </strong>
+              <span>{movie.duration}mins</span>
+            </div>
+
+            <div style={{ marginTop: 16 }}>
+              <Button type="primary">Book Tickets</Button>
+            </div>
           </Col>
         </Row>
       </Card>
